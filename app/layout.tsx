df4de14a2799cc599a39a98e9646411aad4e4978
Nathaniel Alcedo <natealcedo@gmail.com>
2024-06-28 15:53:36 +0800

    Metadata: Add page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import "../main.css";
 
+import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Simple Weather App",
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
---
+++ components/weather-card-now.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { WeatherData } from "@/app/now/page";
+
+interface CardProps {
+  weatherData: WeatherData;
+  additionalClassNames?: string;
+}
+
+export function Card(props: CardProps) {
+  return (
+    <div
+      className={`max-w-sm overflow-hidden rounded-lg bg-gray-300 opacity-80 shadow-md ${props.additionalClassNames}`}
+    >
+      <div className="p-4">
+        <h2 className="mb-2 text-xl font-semibold text-gray-700">{props.weatherData.area}</h2>
+        <p className="text-gray-700">{props.weatherData.forecast}</p>
+      </div>
+    </div>
+  );
+}
