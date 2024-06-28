6c335cdd0d80db32311baa95c5741ffbc850eea7
Nathaniel Alcedo <natealcedo@gmail.com>
2024-06-28 15:58:55 +0800

    Home Page: Add links to correct page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 import Link from "next/link";
 
-import Button from "@/components/button";
+import { Button } from "@/components/button";
 
 export default function Home() {
   return (
     <div className="h-screen bg-[url('./public/background.jpg')] bg-cover bg-center">
       <div className="flex min-h-screen flex-col items-center justify-center">
         <h1 className="pb-24 text-center text-6xl font-bold text-gray-700">Simple Weather App</h1>
-        <div className="flex w-[50%] flex-row gap-24">
-          <Link href="/now">
-            <Button>Page</Button>
+        <div className="flex w-full flex-row justify-center gap-24">
+          <Link href="/now" className="w-[200px]">
+            <Button className="w-full">Now</Button>
           </Link>
 
-          <Link href="/now">
-            <Button>Page</Button>
+          <Link href="/forecast" className="w-[200px]">
+            <Button className="w-full">Forecast</Button>
           </Link>
         </div>
       </div>
