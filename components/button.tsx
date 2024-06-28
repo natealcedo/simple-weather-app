1a355d3c4a296d9010d6e880820eecb163e008c1
Nathaniel Alcedo <natealcedo@gmail.com>
2024-06-28 15:55:10 +0800

    Button: Update button to be more generic

--- components/button.tsx
+++ components/button.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+"use client";
+
+import React from "react";
+
+import { cn } from "@/lib/utils";
 
 interface ButtonProps {
-    title: string;
-    onClick: () => void
+  onClick?: () => void;
+  children?: React.ReactNode;
+  className?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ title, onClick }) => {
-    return (
-        <button className="bg-gray-700 hover:bg-gray-500 text-white text-3xl font-bold py-4 px-6 rounded-full flex-1" onClick={onClick}>
-            {title}
-        </button>
-    );
-};
-
-export default Button;
+export function Button(props: ButtonProps) {
+  return (
+    <button
+      className={cn(
+        "flex-1 rounded-full bg-gray-700 px-6 py-4 text-3xl font-bold text-white hover:bg-gray-500",
+        props.className
+      )}
+      onClick={() => {
+        if (props.onClick) props.onClick();
+      }}
+    >
+      {props.children}
+    </button>
+  );
+}
